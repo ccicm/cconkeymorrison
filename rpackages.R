60040bce209e55dbafa8b3a9110aff749bc415be
Connor Conkey-Morrison <connorconkeymorrison@gmail.com>
2026-03-07 22:43:55 +1100

    update

--- rpackages.R
+++ rpackages.R
@@ -1,2 +1,6 @@
-install.packages("quarto")
-install.packages("rmarkdown")
+install.packages(c("rorcid","rmarkdown","quarto", "dplyr", "glue"))
+library(rorcid)
+orcid_auth()
+
+####ORCID_TOKEN=109922da-dbd1-41e2-b4fd-304599b78ec1
+
